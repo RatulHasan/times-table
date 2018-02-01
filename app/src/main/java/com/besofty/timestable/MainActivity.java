a3ae57d045fe4ef8f8172537634cc17510eaca84
Ratul Hasan <ratuljh@gmail.com>
2018-02-01 10:32:14 +0600

    Initial commit

---
+++ app/src/main/java/com/besofty/timestable/MainActivity.java
@@ -0,0 +1,57 @@
+package com.besofty.timestable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SeekBar timesTableSeekBar = (SeekBar) findViewById(R.id.timesTableSeekBar);
+
+        ListView timesTableListView = (ListView) findViewById(R.id.timesTableListView);
+
+        timesTableSeekBar.setMax(20);
+        timesTableSeekBar.setProgress(10);
+
+        timesTableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                int min = 1;
+                int timesTable;
+                if(i<min){
+                    timesTable=min;
+                    timesTableSeekBar.setProgress(min);
+                }else {
+                    timesTable=i;
+                }
+                Log.i("info", Integer.toString(timesTable));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
+    public void timesTableSeekBar(View view){
+
+    }
+}
